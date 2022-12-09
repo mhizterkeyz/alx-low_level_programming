635833bf54f0b8c0c9951e758ff4b0c9d5fb526e
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2022-12-09 13:25:47 -0800

    add code 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+
+		c += 1;
+	}
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+
+		c += 1;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
