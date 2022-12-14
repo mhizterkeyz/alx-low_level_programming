b6a5034e1e2fd47c33feafa36bda426eedb38ad5
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2022-12-13 22:03:10 -0800

    betty test and fix 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,12 +9,12 @@ int main(void)
 	char __putchar[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	int i;
 
-	for(i = 0; i < 8; i += 1)
+	for (i = 0; i < 8; i += 1)
 	{
 		_putchar(__putchar[i]);
 	}
 
 	_putchar('\n');
 
-	return(0);
+	return (0);
 }
