38ecfd075437c18926d582dfc42d478c1c6205ef
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2022-12-14 01:14:52 -0800

    add code 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * times_table - prints the times table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+
+	a, b = 0;
+	for (; a < 10; a += 1)
+	{
+		for (; b < 10; b += 1)
+		{
+			_putchar('0' + a * b);
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
