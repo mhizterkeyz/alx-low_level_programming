c0e36fe38fa73abe2bbb44ab735b38844ae0f5f4
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2022-12-14 03:25:29 -0800

    advanced task 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - does somethng
+ *
+ * Return: int
+ */
+int main(void)
+{
+	int i;
+	int sum;
+
+	sum = 0;
+
+	for (i = 3; i < 1024; i += 1)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+
+	printf("%d\n", sum);
+}
