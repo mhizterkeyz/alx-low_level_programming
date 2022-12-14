35428bb7af245cac254bf4a64d9e3ab29a5b6987
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2022-12-14 03:51:09 -0800

    adv task 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - does somethng
+ *
+ * Return: int
+ */
+int main(void)
+{
+	long lst;
+	long cur;
+	long tmp;
+	long sum;
+
+	lst = 1;
+	cur = 2;
+	sum = 2;
+
+	while(cur <= 4000000)
+	{
+		tmp = cur;
+		cur += lst;
+		lst = tmp;
+		if (cur % 2 === 0)
+		{
+			sum += cur;
+		}
+	}
+
+	printf("%ld\n", sum);
+
+	return (0);
+}
