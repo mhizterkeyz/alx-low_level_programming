6e502bf8ebd4dd8f774c9c5de443e37606d42e89
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2022-12-13 23:30:09 -0800

    add code 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * jack_bauer - prints each minute in 24 hours
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i < 3; i += 1)
+	{
+		for (j = 0; j < 4; j += 1)
+		{
+			for (k = 0; k < 6; k += 1)
+			{
+				for (l = 0; l < 10; l += 1)
+				{
+					_putchar('0' + i);
+					_putchar('0' + j);
+					_putchar(':');
+					_putchar('0' + k);
+					_putchar('0' + l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
