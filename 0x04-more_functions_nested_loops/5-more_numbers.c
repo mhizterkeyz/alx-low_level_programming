a70b55995917b6b10c2c111519b0d7e6b4a1b5b3
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2022-12-15 12:45:58 -0800

    add code 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * more_numbers - does something
+ *
+ * Return: int
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (i < 10)
+	{
+		while (j < 15)
+		{
+			if (j > 9)
+			{
+				_putchar('0' + j / 10);
+			}
+			_putchar('0' + j % 10);
+		}
+		_putchar('\n');
+	}
+}
