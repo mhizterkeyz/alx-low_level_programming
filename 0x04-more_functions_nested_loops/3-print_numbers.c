724a310415791e4f62c81d39ddb7730999828795
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2022-12-15 12:29:37 -0800

    add code 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - does something
+ *
+ * Return: int
+ */
+
+void print_numbers(void)
+{
+	int l;
+
+	l = 48;
+	while (l <= 57)
+	{
+		_putchar(l);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,5 +5,6 @@ void _putchar(char);
 int _isupper(int);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
 #endif
