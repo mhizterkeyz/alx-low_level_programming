3f91d31e422bed0987882e65e5df61ad51a41552
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2022-12-19 14:48:50 -0800

    pointers 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strlen - gets length of string
+ * @c: char pointer
+ * Return: int
+ */
+int _strlen(char *c)
+{
+	int len;
+
+	len = 0;
+	while (c[len])
+	{
+		len += 1;
+	}
+
+	return (len);
+}
+
+/**
+ * puts_half - does something
+ * @str: char
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len;
+	int srt;
+
+	len = _strlen(s);
+	srt = len - (len / 2);
+
+	while (srt < len)
+	{
+		_putchar(s[srt]);
+		srt += 1;
+	}
+	_putchar('\n');
+}
