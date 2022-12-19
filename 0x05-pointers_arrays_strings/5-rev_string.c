79b77f2b0f27bfb1a5706e36fed2f17971c1b39c
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2022-12-19 15:39:36 -0800

    pointers 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - does something
+ * @s: param
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len;
+	int i;
+
+	while(s[len])
+	{
+		len += 1;
+	}
+
+	for (i = 0; i < len / 2; i += 1)
+	{
+		char tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
+	}
+}
