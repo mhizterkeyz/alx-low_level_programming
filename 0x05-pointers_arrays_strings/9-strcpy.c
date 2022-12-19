a95bf35f989087eaef90f8b4ecfd84793a445beb
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2022-12-19 15:17:24 -0800

    pointers 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcpy - does something
+ * @dest: param
+ * @src: param
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
