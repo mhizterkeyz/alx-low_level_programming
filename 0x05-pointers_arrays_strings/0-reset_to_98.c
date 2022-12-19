4797c1159934ad5a827dc7d298335a42dfbb7b12
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2022-12-19 13:33:36 -0800

    pointers 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets to 98
+ * @n: point to int
+ * Returns: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+void _putchar(char);
+void reset_to_98(int *n);
+
+#endif
