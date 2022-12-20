67d63b3a529e99652c5313fcc7c87325b15f958f
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2022-12-19 17:02:55 -0800

    chore

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,13 +9,14 @@
  */
 int main(void)
 {
+	char factory[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char str[58];
 	int i;
 
 	srand(time(0));
 	for (i = 0; i < 57; i += 1)
 	{
-		str[i] = 'a' + rand() % 26;
+		str[i] = factory[rand() % 62];
 	}
 	str[57] = '\0';
 	printf("%s\n", str);
