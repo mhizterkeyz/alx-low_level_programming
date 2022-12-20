ab2e2eac1abd5e14109131d9442f32a9e3fdfd43
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2022-12-19 16:34:24 -0800

    pointers adv 1

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _atoi - does something
+ * @s: param
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int res = 0;
+	int sign = 1;
+	int started = 0;
+
+	while (s[i])
+	{
+		if (s[i] == 45)
+		{
+			sign = -1;
+		}
+
+		while (48 <= s[i] && s[i] <= 57)
+		{
+			started = 1;
+			res = (res * 10) + (s[i] - '0');
+			i += 1;
+		}
+
+		if (started)
+		{
+			break;
+		}
+		i += 1;
+	}
+
+	return (res * sign);
+}
