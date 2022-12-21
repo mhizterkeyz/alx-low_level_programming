09377e13e4b781ff06e6de473464e9dfb05e1074
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2022-12-21 14:27:19 -0800

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - does something
+ * @dest: param
+ * @src: param
+ * Return: something
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (dest[i])
+	{
+		i += 1;
+	}
+
+	while (j < n && src[j])
+	{
+		dest[i] = src[j];
+		i += 1;
+		j += 1;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
