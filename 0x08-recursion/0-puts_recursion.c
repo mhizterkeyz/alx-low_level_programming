c5971e5efe9ad210887cdea1ee9f3031d8f7a5db
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2023-01-04 08:48:05 -0800

    add some code

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print - does something
+ * @s: param
+ * @p: param
+ * Return: void
+ */
+void print(char *s, int p)
+{
+	if (!s[p])
+	{
+		return;
+	}
+
+	_putchar(s[p]);
+	print(s, p++);
+}
+
+/**
+ * _puts_recursion - does something
+ * @s: param
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	print(s, 0);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+void _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
