e64ceee4e06d09a326debc113b6e54bf0f093d3b
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2023-01-04 09:30:18 -0800

    add some code

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - does something
+ * @n: param
+ * Return: int
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
+}
