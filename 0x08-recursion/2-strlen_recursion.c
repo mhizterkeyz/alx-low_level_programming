53d45b9201ee4c55fa02ceb59b514f6b54e78607
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2023-01-04 09:21:31 -0800

    add some code

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _count_it - does something
+ * @s: param
+ * @c: param
+ * Return: int
+ */
+int _count_it(char *s, int c)
+{
+	if (!s[c])
+	{
+		return c;
+	}
+	return _count_it(s, c + 1);
+}
+
+/**
+ * _strlen_recursion - does something
+ * @s: param
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	return _count_it(s, 0);
+}
