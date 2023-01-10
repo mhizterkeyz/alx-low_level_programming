844584ed97da63f06565202a32ee21d9f9fc09fb
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2023-01-10 07:35:37 -0800

    meet requirements

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,11 @@ char *create_array(unsigned int size, char c)
 	char *p = malloc(sizeof(c) * size);
 	unsigned int i = 0;
 
+	if (p == NULL || size < 1)
+	{
+		return NULL;
+	}
+
 	while (i < size)
 	{
 		p[i] = c;
