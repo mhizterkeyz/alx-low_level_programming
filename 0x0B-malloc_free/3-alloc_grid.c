cb304576db8794280b22036c8c9d0303e3e85852
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2023-01-10 10:47:41 -0800

    omo

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -48,7 +48,7 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
-			free_grid(grid);
+			free_grid(grid, i);
 			return (NULL);
 		}
 		k = 0;
