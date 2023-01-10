68d58c61239453d3022f178c959bfc3fd17c9a05
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2023-01-10 08:25:36 -0800

    add code

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * str_concat - does something
+ * @s1: param
+ * @s2: param
+ * Return: char
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *res;
+	int i;
+	int j;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	res = malloc(strlen(s1) + strlen(s2) + 1);
+	i = 0;
+	j = 0;
+
+	while (s1[i])
+	{
+		res[j] = s1[i];
+		i += 1;
+		j += 1;
+	}
+
+	i = 0;
+
+	while (s2[i])
+	{
+		res[j] = s2[j];
+		i += 1;
+		j += 1;
+	}
+	res[j] = '\0';
+
+	return (res);
+}
