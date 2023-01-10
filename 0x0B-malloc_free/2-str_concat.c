b71c27a6ee4317b30fbb79eaa8853c0d072f08fa
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2023-01-10 08:27:36 -0800

    handle malloc error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -27,6 +27,11 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	j = 0;
 
+	if (res == NULL)
+	{
+		return (NULL);
+	}
+
 	while (s1[i])
 	{
 		res[j] = s1[i];
@@ -38,7 +43,7 @@ char *str_concat(char *s1, char *s2)
 
 	while (s2[i])
 	{
-		res[j] = s2[j];
+		res[j] = s2[i];
 		i += 1;
 		j += 1;
 	}
