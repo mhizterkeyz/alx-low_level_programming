7145dab2286f3ea431e7e5feab5b6d7650a4efd6
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2023-01-18 06:57:16 -0800

    linting fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,28 +1,29 @@
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 #include "3-calc.h"
 
 /**
  * get_op_func - does something
  * @s: param
+ * Return: int
  */
 int (*get_op_func(char *s))(int, int)
 {
-  op_t ops[] = {
-      {"+", op_add},
-      {"-", op_sub},
-      {"*", op_mul},
-      {"/", op_div},
-      {"%", op_mod}};
-  int i = 0;
+	op_t ops[] = {
+			{"+", op_add},
+			{"-", op_sub},
+			{"*", op_mul},
+			{"/", op_div},
+			{"%", op_mod}};
+	int i = 0;
 
-  while (i < 5)
-  {
-    if (strcmp(s, ops[i].op) == 0)
-    {
-      return (ops[i].f);
-    }
-    i += 1;
-  }
-  return (NULL);
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (ops[i].f);
+		}
+		i += 1;
+	}
+	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,30 +4,32 @@
 
 /**
  * main - does something
+ * @argc: param
+ * @argv: param
  * Return: int
  */
 int main(int argc, char *argv[])
 {
-  int (*func)(int, int);
-  int ans;
+	int (*func)(int, int);
+	int ans;
 
-  if (argc != 4)
-  {
-    printf("Error\n");
-    return (98);
-  }
-  func = get_op_func(argv[2]);
-  if (!func)
-  {
-    printf("Error\n");
-    return (99);
-  }
-  ans = func(atoi(argv[1]), atoi(argv[3]));
-  if (ans == '\0')
-  {
-    printf("Error\n");
-    return (100);
-  }
-  printf("%d\n", ans);
-  return (0);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	func = get_op_func(argv[2]);
+	if (!func)
+	{
+		printf("Error\n");
+		return (99);
+	}
+	ans = func(atoi(argv[1]), atoi(argv[3]));
+	if (ans == '\0')
+	{
+		printf("Error\n");
+		return (100);
+	}
+	printf("%d\n", ans);
+	return (0);
 }
