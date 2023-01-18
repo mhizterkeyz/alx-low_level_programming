e685ee12154d5621a3e2d9465949dfc036af3941
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2023-01-18 03:35:52 -0800

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - does something
+ * @array: param
+ * @size: param
+ * @action: param
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	for (; i < size; i += 1)
+	{
+		action(array[i]);
+	}
+}
