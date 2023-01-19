b07d6a171abc3da6ea916dfd83734d25a295d02f
Emmanuel Menyaga <emmanuelmenyaga.ojay@gmail.com>
2023-01-19 06:15:07 -0800

    fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,9 +7,9 @@
  * @action: param
  * Return: void
  */
-void array_iterator(int *array, size_t size, void (*action)(int))
+void array_iterator(int *array, unsigned int size, void (*action)(int))
 {
-	size_t i = 0;
+	unsigned int i = 0;
 
 	for (; i < size; i += 1)
 	{
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,9 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-#include <stddef.h>
 
 void _putchar(char);
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, unsigned int size, void (*action)(int));
 
 #endif
